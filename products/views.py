6635b3e97161b894274f5214d1df6f6d2e4d2fa8
gitgroch <geoff.rochford@gmail.com>
2023-03-01 12:05:30 +0000

    Add sorting

--- products/views.py
+++ products/views.py
@@ -11,9 +11,26 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     # check if there is a request
     if request.GET:
+        # Check if sort is in request.GET
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            # Check for direction 
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+
         # Check for category query in request
         if 'category' in request.GET:
             # If exists convert to list 
@@ -35,11 +52,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-    }
+        'current_sorting' : current_sorting,    }
 
     return render(request, 'products/products.html', context)
 
