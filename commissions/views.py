25e4bc223d7e4a3dee82d9b371c3aea6deb38cb6
gitgroch <geoff.rochford@gmail.com>
2023-03-27 15:04:33 +0000

    Fix crispy form syntax in commissions form html so the form submits

--- commissions/views.py
+++ commissions/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .forms import CommissionForm
 from .models import Commission
 
 
 # Create your views here.
-def commissions(request):
-    """ A view to return the commissions form page """
-    form = CommissionForm()
-    return render(request, 'commissions/commissions_form.html', {'form': form})
+# def commissions(request):
+#     """ A view to return the commissions form page """
+#     form = CommissionForm()
+#     return render(request, 'commissions/commissions_form.html', {'form': form})
 
+def commissions(request):
+    if request.method == "POST":
+        form = CommissionForm(request.POST)
+        if form.is_valid():    
+            form = form.save(commit=False)
+            form.save()
+            # return HttpResponseRedirect(reverse('home'))
+            return render(request, 'commissions/post_submit_confirm.html',)
+    else:
+        form = CommissionForm()
+    return render(request, 'commissions/commissions_form.html', {'form': form})
